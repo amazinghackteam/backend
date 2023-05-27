b1047ab2346f3872b50a961c65579268f229e5d2
Your Name <you@example.com>
2023-05-27 11:51:14 +0800

    feat: add transfer func

--- LiquidZeroDogToken/lib.rs
+++ LiquidZeroDogToken/lib.rs
@@ -7,16 +7,53 @@ mod LiquidZeroDogToken {
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(Default)]
     pub struct LiquidZeroDogToken {
         /// Stores a single `bool` value on the storage.
-        value: bool,
+        total_supply: Balance,
+        balances: Mapping<AccountId, Balance>,
+    }
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
     }
 
     impl LiquidZeroDogToken {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+       #[ink(constructor)]
+        pub fn new(total_supply: Balance) -> Self {
+            let mut balances = Mapping::default();
+            let caller = Self::env().caller();
+            balances.insert(caller, &total_supply);
+            Self {
+                total_supply,
+                balances,
+            }
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<(), Error> {
+            let from = self.env().caller();
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, &(to_balance + value));
+            Ok(())
         }
 
         /// Constructor that initializes the `bool` value to `false`.
@@ -49,6 +86,32 @@ mod LiquidZeroDogToken {
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+
+        #[ink::test]
+        fn total_supply_works() {
+            let LiquidZeroDogToken = LiquidZeroDogToken::new(100);
+            assert_eq!(LiquidZeroDogToken.total_supply(), 100);
+        }
+
+        #[ink::test]
+        fn balance_of_works() {
+            let LiquidZeroDogToken = LiquidZeroDogToken::new(100);
+            let accounts =
+                ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+            assert_eq!(LiquidZeroDogToken.balance_of(accounts.alice), 100);
+            assert_eq!(LiquidZeroDogToken.balance_of(accounts.bob), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut LiquidZeroDogToken = LiquidZeroDogToken::new(100);
+            let accounts =
+                ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+
+            assert_eq!(LiquidZeroDogToken.balance_of(accounts.bob), 0);
+            assert_eq!(LiquidZeroDogToken.transfer(accounts.bob, 10), Ok(()));
+            assert_eq!(LiquidZeroDogToken.balance_of(accounts.bob), 10);
+        }
 
         /// We test if the default constructor does its job.
         #[ink::test]
